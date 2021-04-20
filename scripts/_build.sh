6f2992aab5f0506e2c40143cb6e82459484f8972
Joseph Bayly <jtbayly@gmail.com>
2021-04-20 14:20:33 -0400

    this time?

--- scripts/_build.sh
+++ scripts/_build.sh
@@ -2,8 +2,10 @@
 
 set -ev
 tlmgr option repository ctan
+tlmgr repository list
+curl -fsSL https://www.preining.info/rsa.asc | tlmgr key add -
 wget http://mirror.ctan.org/systems/texlive/tlnet/update-tlmgr-latest.sh
-./update-tlmgr-latest.sh
+sudo ./update-tlmgr-latest.sh
 
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
