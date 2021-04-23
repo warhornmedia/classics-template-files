927856f722c77cd760b5136220d5cf42a40aa3c6
Joseph Bayly <jtbayly@gmail.com>
2021-04-23 00:38:47 -0400

    add path to file

--- scripts/_buildGHA.sh
+++ scripts/_buildGHA.sh
@@ -5,7 +5,7 @@ set -ev
 # Install fonts
 
 # encrypted via: "gpg -c font1.zip"
-echo NotASecret | gpg --batch --yes --passphrase-fd 0 -d --output font1.zip font1.zip.gpg
+echo NotASecret | gpg --batch --yes --passphrase-fd 0 -d --output classics-template-files/fonts/font1.zip classics-template-files/fonts/font1.zip.gpg
 unzip classics-template-files/fonts/font1.zip
 unzip classics-template-files/fonts/font2.zip
 sudo cp -vf Calluna/Calluna-Regular.otf /Library/Fonts
