e1d68a0adab68ab8d2ea34e38a20f0ea7267fa9a
Joseph Bayly <jtbayly@gmail.com>
2021-04-23 00:34:15 -0400

    encrypt font1

--- scripts/_buildGHA.sh
+++ scripts/_buildGHA.sh
@@ -3,6 +3,9 @@
 set -ev
 
 # Install fonts
+
+# encrypted via: "gpg -c font1.zip"
+echo NotASecret | gpg --batch --yes --passphrase-fd 0 -d --output font1.zip font1.zip.gpg
 unzip classics-template-files/fonts/font1.zip
 unzip classics-template-files/fonts/font2.zip
 sudo cp -vf Calluna/Calluna-Regular.otf /Library/Fonts
