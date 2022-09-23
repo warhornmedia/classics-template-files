cb687273e512011ec3ae1aa394eb32a4125e7089
Joseph Bayly <jtbayly@gmail.com>
2022-09-23 11:42:55 -0400

    add lua filter to show unnumbered sections and chapters in PDF headers

---
+++ filters/filter_unnumbered_content.lua
@@ -0,0 +1,22 @@
+--- Removes notes and links
+local function clean (inlines)
+  return inlines:walk {
+    Note = function (_) return {} end,
+    Link = function (link) return link.content end,
+  }
+end
+
+--- Creates an Inlines singleton containing the raw LaTeX.
+local function l(text)
+  return pandoc.Inlines{pandoc.RawInline('latex', text)}
+end
+
+function Header (h)
+  if h.level <= 2 and h.classes:includes 'unnumbered' then
+    local title = clean(h.content)
+    local secmark = h.level == 1
+      and l'\\markboth{' .. title .. l'}{' .. title .. l'}'
+      or l'\\markright{' .. title .. l'}' -- subsection, keep left mark unchanged
+    return {h, secmark}
+  end
+end
